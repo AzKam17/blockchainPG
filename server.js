e51cea4c0ddad5405af682f3d8310daa81374d13
AzKam <azizkamadou17@gmail.com>
2022-04-01 01:37:50 +0000

    Activation Blockchain

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const crypto = require('crypto'), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+const crypto = require('crypto'), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
 
 const {Block, Blockchain} = require('./Block');
 const {Transaction} = require('./Transaction');
@@ -6,13 +6,13 @@ const EC = require("elliptic").ec, ec = new EC("secp256k1");
 
 // Your original balance is 100000
 const holderKeyPair = ec.genKeyPair();
-let JeChain = new Blockchain(holderKeyPair);*/
+let JeChain = new Blockchain(holderKeyPair);
 
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send(JSON.stringify(JeChain.chain))
 })
 
 app.listen(process.env.PORT || 8080, () => {
